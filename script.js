fe77fd3de1e010ecae82244c212ba759d06d9256
royweb <xytank@gmail.com>
2021-09-21 13:27:49 -0400

    Set up script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const tallestAnimals = [
+    "Giraffe",
+    "African Elephant",
+    "SIBERIAN TIGER",
+    "Liger",
+    "Polar Bear",
+    "Asian Elephant",
+    "Bengal Tiger",
+    "Moose",
+    "Grizzly Bear",
+    "Ostrich",
+];
+
+// Store listitems
+const listItems = [];
