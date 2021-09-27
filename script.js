848a980eeae809258af33d6b0e808e6c36997b35
royweb <xytank@gmail.com>
2021-09-27 12:57:59 -0400

    Add drag and drop logic

--- script.js
+++ script.js
@@ -52,23 +52,36 @@ function createList() {
 }
 
 function dragStart() {
-    console.log('Event: ', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
 }
 
 function dragEnter() {
-    console.log('Event: ', 'dragenter');
+    // 'this' refer to the current element
+    this.classList.add('over');
 }
 
 function dragLeave() {
-    console.log('Event: ', 'dragleave');
+    this.classList.remove('over');
 }
 
-function dragOver() {
-    console.log('Event: ', 'dragover');
+function dragOver(e) {
+    e.preventDefault();
 }
 
 function dragDrop() {
-    console.log('Event: ', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+  
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    // Get the DOM elements
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
 }
 
 function addEventListeners() {
